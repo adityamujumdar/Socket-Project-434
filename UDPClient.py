a3710b7641fdcc6cf62367fcea369679d986be31
adityamujumdar <47087525+adityamujumdar@users.noreply.github.com>
2020-09-18 18:11:06 -0700

    Update UDPClient.py
    initial testing commit 2/3

--- UDPClient.py
+++ UDPClient.py
@@ -3,7 +3,7 @@ import sys
 
 HOST = 'localhost'
 PORT = 8888
-BUFFER_SIZE = 1024
+BUFFER_SIZE = 4096
 ITERATIONS = 5
 
 try:
@@ -15,20 +15,18 @@ except socket.error:
 
 for i in range(ITERATIONS):
 	message = input('Enter message to send: ')
-	print('Client sends ', message, ' to server')
+	print('Client sends', message, 'to server')
 
 	try:
 		socket_t.sendto(message.encode('utf-8'), (HOST, PORT))
 
 		# Receive data from server (data, addr)
-		received_d = socket_t.recvfrom(BUFFER_SIZE)
-		reply = received_d[0]
-		addr = received_d[1]
+		reply, server_addr = socket_t.recvfrom(BUFFER_SIZE)
 
 		print('Server replied: ', reply)
 
 	except socket.error as message:
-		print('Error Code: '+str(message[0])+' Message ' + message[1])
+		print('Error Code : ' + str(message[0]) + ' Message ' + message[1])
 		sys.exit()
 
 socket_t.close()
